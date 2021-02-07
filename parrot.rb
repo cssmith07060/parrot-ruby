d1c42ad9af0e0cd82b21428ed7a90cee50bae6b6
cssmith07060 <66744380+cssmith07060@users.noreply.github.com>
2021-02-07 17:17:55 -0500

    Add files via upload

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,25 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot
+ puts "Squawk!"
+end
+parrot
+
+def parrot(phrase = "Squawk!")
+    puts phrase 
+   phrase
+end
+parrot
+
+def parrot(phrase = "Squawk!")
+    puts phrase
+end
+parrot("Pretty bird!")
+
+def parrot(phrase = "Squawk!")
+    puts phrase
+    "Pretty bird!"
+end
+parrot("Pretty bird!")
+
+
